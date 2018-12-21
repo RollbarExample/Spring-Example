fa5e1c4b7cf4ad1bda30a3e2812acc2bfcf47fe6
RollbarExample <wahid.ali@psquickit.com>
2018-12-22 01:21:31 +0530

    Initial commi

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -23,24 +23,19 @@ import com.rollbar.notifier.config.ConfigBuilder;
 @RestController
 public class UserController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
-	
+
 	Rollbar rollbar;
-	
-	@RequestMapping("/rollbar/Testing")
-	public String testing() {
+
+	@RequestMapping("/rollbar/generateError")
+	public String generateError() {
 		setupRollbar();
 		String test = "Test";
 		try {
 			test.toString();
 		} catch (Exception e) {
 			e.printStackTrace();
-			 HashMap<String,Object> map=new HashMap<String, Object>();
-			 map.put("Id","123");
-			 map.put("User Name","John Doe");
-			 map.put("Email","john@doe.com");
-			 rollbar.error(e,map);
-			
-			return "Exception : "+e.getMessage();
+			rollbar.error(e);
+			return "Exception : " + e.getMessage();
 		}
 		return "SUCCESS";
 	}
@@ -53,68 +48,4 @@ public class UserController {
 
 	}
 
-	@Autowired
-	private UserService repository;
-
-	@RequestMapping("/auth")
-	public LoginResponse authUser(@RequestHeader(value = "authToken") String authToken) {
-		LoginResponse response = new LoginResponse();
-		setupRollbar();
-		if (!Constant.authTken.equalsIgnoreCase(authToken)) {
-			response.setErrorCode(Constant.INVALID_TOKEN_CODE + "");
-			response.setErrorMsg(Constant.INVALID_TOKEN_MSG);
-			LOGGER.error(Constant.INVALID_TOKEN_MSG);
-			return response;
-		}
-		User user = new User();
-		user.setName("Test User");
-		user.setAge("25");
-		response.setStatus(true);
-		response.setErrorCode("SUCCESS");
-		response.setErrorMsg("Success");
-		response.setUserDetail(user);
-		return response;
-	}
-
-	@PostMapping("/addUser")
-	public GenericResponse createQuestion(@Valid @RequestBody User request) {
-		GenericResponse response = new GenericResponse();
-		try {
-			UserEntity entity = new UserEntity();
-			entity.setAge(request.getAge());
-			entity.setName(request.getName());
-			repository.save(entity);
-		} catch (Exception e) {
-			// TODO: handle exception
-			LOGGER.error("Invalid sql query", e);
-		}
-		return response;
-	}
-
-	@RequestMapping("/rollbar/Production")
-	public LoginResponse authUser() {
-		setupRollbar();
-		LoginResponse response = new LoginResponse();
-		String test=null;
-		try {
-			test.toString();
-		} catch (Exception e) {
-			// TODO: handle exception
-			rollbar.error(e);
-		}
-		return response;
-	}
-	@RequestMapping("/rollbar/Staging")
-	public LoginResponse staging() {
-		setupRollbar();
-		LoginResponse response = new LoginResponse();
-		String[] test= {"Test1"};
-		try {
-			String testString=test[1];
-		} catch (Exception e) {
-			rollbar.error(e);
-		}
-		return response;
-	}
-	
 }
